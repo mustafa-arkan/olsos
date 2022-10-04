ad77190f0d7a07fe8f7903bf690f4c66fa9b65d0
mustafa-arkan <arkancse@gmail.com>
2022-10-04 10:52:48 +0600

    calculation done

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+
+    const {cart}=props
+
+    let total=0
+    let shippingCost=0
+    for(const cost of cart){
+
+
+total=total+cost.price
+shippingCost=shippingCost+cost.shipping
+
+    }
+    const tax=parseFloat((total*0.1).toFixed(2))
+    const grandTotal=total+shippingCost+tax
+
+
+
     return (
-        <div>
+        <div className='cart'>
             <h4>Order Summery</h4>
 <p>Selected item: {cart.length}</p>
+<p>Total Price:${total}  </p>
+<p>Total Shipping:${shippingCost} </p>
+<p>Tax:${tax} </p>
+<h4>Grand Total: ${grandTotal.toFixed(2)} </h4>
+
         </div>
     );
 };
